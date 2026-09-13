b1ffd8d984b33b8cb21790420b387ca213df2d99
Rohaan <102744858+ROH-0000@users.noreply.github.com>
2026-09-13 12:51:59 +0100

    Setup bare bones discord interaction

--- src/TvTracker.Host/Program.cs
+++ src/TvTracker.Host/Program.cs
@@ -1,4 +1,17 @@
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services.ApplicationCommands;
+
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Services
+	.AddDiscordGateway(options =>
+	{
+		options.Token = builder.Configuration["Discord:Token"];
+	})
+	.AddApplicationCommands();
+
 var host = builder.Build();
-host.Run();
+
+host.AddSlashCommand("square", "Square!", (int a) => $"{a}² = {a * a}");
+
+await host.RunAsync();
